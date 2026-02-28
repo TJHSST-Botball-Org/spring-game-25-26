63313cfd62b165b07bffb3929b7a74f878ef50bb
Ronit Singh <102002190+ArkXero@users.noreply.github.com>
2026-02-28 16:33:01 -0500

    update nano to agastya's new version
    Added a function to turn the robot right by 90 degrees based on orientation.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,37 @@
 #include <kipr/kipr.h>
 #include <nano.h>
 #include <iostream>
+#include "shared.h"
+
+std::atomic<double> orientation(0);
+
+void turn_right_90(){
+	double o = orientation;
+    while((orientation-o)<90){
+     	std::cout << orientation + " - turning right" <<std::endl;   
+    }
+}
 
 int main() {
+    Nano::start_nano();
     Nano::BaseRobot robot;
     
     std::cout << "Robot created!" << std::endl;
     
+    robot.calibrate_gyro();
+    robot.get_gyro_x();
+    turn_right_90();
+    //while(true){
+     //	std::cout << orientation << std::endl; 
+        //msleep(10);
+    //}
     // Move motor 0
-    robot.set_motor_power(0, 100);
+    //robot.set_motor_power(0, 100);
     //robot.set_motor_power(1, 100);
-    msleep(1000);
+    //msleep(1000);
     //robot.wait_for_milliseconds(1000);
-    robot.set_motor_power(0, 0);
-    msleep(10000);
+    //robot.set_motor_power(0, 0);
+    //msleep(10000);
     
     std::cout << "Done!" << std::endl;
     
