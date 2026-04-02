06bce27a6d5a7feff6b6d6286b78d486db49ff2a
AgastyaMittal <77212425+agastyamittal@users.noreply.github.com>
2026-04-02 09:29:02 -0400

    update to nano

--- shared.h
+++ shared.h
@@ -5,5 +5,10 @@
 #include <mutex>
 
 extern std::atomic<double> orientation;
-
+extern std::atomic<double> pix;
+extern std::atomic<double> piy;
+extern std::atomic<double> piz;
+extern std::atomic<bool> nr;
+extern std::atomic<bool> nr2;
+extern std::atomic<bool> nr3;
 #endif
